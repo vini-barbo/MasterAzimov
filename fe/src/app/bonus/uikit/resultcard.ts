fffe789a3c1a1c75e2811c4a9fcf1f5254c83feb
vini-barbo <vinicius2508@hotmail.com>
2025-02-19 13:44:41 -0300

    refactor

---
+++ fe/src/app/bonus/uikit/resultcard.ts
@@ -0,0 +1,51 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface IResultCard {
+    title?: string | null | undefined;
+    label?: string | null | undefined;
+    value?: number | null | undefined;
+    bgColor?: string | null | undefined;
+    iconUrl?: string | null | undefined;
+    iconAlt?: string | null | undefined;
+    textColor?: string | null | undefined;
+    loading?: boolean | null | undefined;
+
+}
+
+@Component({
+    standalone: true,
+    selector: 'app-resultcard',
+    imports: [CommonModule],
+    template: `
+    <div class="col-span-12 lg:col-span-6 xl:col-span-3 ">
+            <div class="card mb-0" [style]="'background-color:'+bgColor +'!important'">
+                <div class="flex flex-row-reverse justify-between mb-4">
+                    <div class="flex flex-col justify-between">
+                        <span class="block  font-medium mb-4 text-white  text-xl" [style]="'color:'+textColor" >{{label || ''}}</span>
+                        <div class=" dark:text-surface-0 font-medium text-4xl  text-center " [style]="'color:'+textColor" >{{value || '-'}}</div>
+                    </div>
+
+                        <div class="h-20 w-28 bg-red-50 p-2 rounded-md">
+                            <img [src]="iconUrl" [alt]="iconAlt" class="w-full h-full" >
+                        </div>
+                </div>
+                <span class=" font-medium text-2xl " [style]="'color:'+textColor">{{title}}</span>
+            </div>
+    </div>
+`
+})
+export class ResultCard {
+
+    @Input() title: string | null | undefined = '';
+    @Input() label: string | null | undefined = '';
+    @Input() value: number | null | undefined = null;
+    @Input() bgColor: string | null | undefined = '#ffffff';
+    @Input() textColor: string | null | undefined = '#000000';
+    @Input() iconUrl: string | null | undefined = '';
+    @Input() iconAlt: string | null | undefined = '';
+    @Input() loading: boolean | null | undefined = false;
+
+
+}
+
--- fe/src/app/pages/review/phase-two/phase-two.component.ts
+++ fe/src/app/pages/review/phase-two/phase-two.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { FluidModule } from 'primeng/fluid';
@@ -6,36 +6,44 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 import { TextareaModule } from 'primeng/textarea';
 import { ArticlesService } from '../../../api/articles.service';
-import { ArticleTable } from "./articles-table.component";
 import { IEsummaryResultItem } from '../../../api/types/IEsummary';
 import { CommonModule } from '@angular/common';
+import { ResultCard, IResultCard } from "../../../bonus/uikit/resultcard";
+import { DatePickerModule } from 'primeng/datepicker';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
     selector: 'app-phase-two',
     templateUrl: './phase-two.component.html',
     providers: [ArticlesService],
-    imports: [InputTextModule, FluidModule, ButtonModule,
-        SelectModule, FormsModule, TextareaModule, ArticleTable, CommonModule],
+    imports: [InputTextModule, FluidModule, ButtonModule, DatePickerModule, ToggleSwitchModule,
+        SelectModule, FormsModule, TextareaModule, CommonModule, ResultCard],
 })
 export class PhaseTwoComponent implements OnInit {
     constructor(private readonly articleService: ArticlesService) { }
 
+
+    articleSourceFoundResult: WritableSignal<IResultCard[]> = signal([
+        { title: 'IEEE', iconAlt: 'IEEE', label: 'Articles', value: 23, bgColor: '#0d9467', iconUrl: 'assets/images/ieee-logo.svg', textColor: '#ffffff' },
+        { title: 'PUBMED', iconAlt: 'PUBMED', label: 'Articles', value: 23, bgColor: '#5273bb', iconUrl: 'assets/images/pubmed-logo.svg', textColor: '#ffffff' },
+    ]);
+
     formData = { text: '' };
     submittedData!: IEsummaryResultItem[];
-    loadingTable: boolean = false;
+    loading: boolean = false;
 
     onSubmit(): void {
-        this.loadingTable = true;
+        this.loading = true;
 
         this.articleService.getArticlesSummary(this.formData.text).subscribe({
             next: (response: IEsummaryResultItem[]) => {
                 this.submittedData = response;
                 console.log('Data fetched successfully:', response);
-                this.loadingTable = false;
+                this.loading = false;
             },
             error: (error) => {
                 console.error('Error fetching data:', error);
-                this.loadingTable = false;
+                this.loading = false;
             },
         });
     }
